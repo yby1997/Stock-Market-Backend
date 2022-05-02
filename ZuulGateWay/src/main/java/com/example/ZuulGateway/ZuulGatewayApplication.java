4011534bc749fc63c7452e2a796d2bf174168dc3
yby1997 <frmpjmattlemage@gmail.com>
2022-05-01 22:06:28 -0700

    added zuul

---
+++ ZuulGateWay/src/main/java/com/example/ZuulGateway/ZuulGatewayApplication.java
@@ -0,0 +1,36 @@
+package com.example.ZuulGateway;
+
+import com.example.ZuulGateway.filter.ErrorFilter;
+import com.example.ZuulGateway.filter.PostFilter;
+import com.example.ZuulGateway.filter.PreFilter;
+import com.example.ZuulGateway.filter.RouteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+
+}
---
+++ ZuulGateWay/src/main/java/com/example/ZuulGateway/filter/RouteFilter.java
@@ -0,0 +1,28 @@
+package com.example.ZuulGateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RouteFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "route";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        System.out.println("Inside Route Filter");
+        return null;
+    }
+}
